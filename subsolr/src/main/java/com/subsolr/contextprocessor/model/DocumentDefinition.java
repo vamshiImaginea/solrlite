ff8f00870e2c97dbf50c96bed2bb4a63c701a47c
vamshiImaginea <vamshi.y@imaginea.com>
2014-01-27 17:02:13 +0530

    working code

--- subsolr/src/main/java/com/subsolr/contextprocessor/model/DocumentDefinition.java
+++ subsolr/src/main/java/com/subsolr/contextprocessor/model/DocumentDefinition.java
@@ -1,33 +1,26 @@
 package com.subsolr.contextprocessor.model;
 
+import static com.googlecode.cqengine.query.QueryFactory.contains;
+
 import java.lang.reflect.InvocationTargetException;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
+import java.util.Set;
 
 import javassist.CannotCompileException;
 import javassist.NotFoundException;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import com.googlecode.cqengine.IndexedCollection;
+import com.googlecode.cqengine.attribute.SimpleAttribute;
+import com.googlecode.cqengine.resultset.ResultSet;
 import com.subsolr.entityprocessors.model.Record;
 import com.subsolr.util.DynamicIndexer;
 import com.subsolr.util.PojoGenerator;
-import com.googlecode.cqengine.CQEngine;
-import com.googlecode.cqengine.IndexedCollection;
-import com.googlecode.cqengine.attribute.SimpleAttribute;
-import com.googlecode.cqengine.index.hash.HashIndex;
-import com.googlecode.cqengine.index.navigable.NavigableIndex;
-import com.googlecode.cqengine.index.radixreversed.ReversedRadixTreeIndex;
-import com.googlecode.cqengine.index.suffix.SuffixTreeIndex;
-import com.googlecode.cqengine.query.Query;
-import com.googlecode.cqengine.resultset.ResultSet;
-
-import static com.googlecode.cqengine.query.QueryFactory.*;
-
 
 /**
  * Contains the possible lucene document definitions using field name -- source
@@ -40,7 +33,7 @@ public class DocumentDefinition {
 	private String documentName;
 	private Map<String, FieldSetDefinition> fieldSets;
 	LinkedHashMap<String, String> mappingRules;
-	;
+	Map<String,Set<String>> attributes = Maps.newHashMap(); // for joins
 
 	public String getDocumentName() {
 		return documentName;
@@ -58,7 +51,8 @@ public class DocumentDefinition {
 		this.fieldSets = map;
 	}
 
-	public List<Record> getRecordsToBeIndexed() throws NotFoundException, CannotCompileException, IllegalArgumentException, SecurityException, InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+	public List<Record> getRecordsToBeIndexed() throws NotFoundException, CannotCompileException, IllegalArgumentException, SecurityException, InstantiationException, IllegalAccessException,
+			InvocationTargetException, NoSuchMethodException {
 		Map<String, List<Record>> recordsByFieldSet = Maps.newHashMap();
 		for (Map.Entry<String, FieldSetDefinition> fieldSetEntry : fieldSets.entrySet()) {
 			recordsByFieldSet.put(fieldSetEntry.getKey(), fieldSetEntry.getValue().getEntityProcessor().getRecords(fieldSetEntry.getValue()));
@@ -67,71 +61,118 @@ public class DocumentDefinition {
 		return combinedFieldSets(recordsByFieldSet);
 
 	}
-	@SuppressWarnings({"rawtypes","unchecked"})
-	private List<Record> combinedFieldSets(Map<String, List<Record>> recordsByFieldSet) throws NotFoundException, CannotCompileException, IllegalArgumentException, SecurityException, InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	private List<Record> combinedFieldSets(Map<String, List<Record>> recordsByFieldSet) throws NotFoundException, CannotCompileException, IllegalArgumentException, SecurityException,
+			InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+		List<Record> records = Lists.newArrayList();
 		for (Map.Entry<String, String> mappingRuleEntry : mappingRules.entrySet()) {
+			records = Lists.newArrayList();
 			String mappingRule = mappingRuleEntry.getValue();
+			attributes.put(mappingRuleEntry.getKey(), Sets.<String>newHashSet());
 			final String[] fieldSetConditions = mappingRule.split("=");
-			String[] fieldSetsInMapping = fieldSetConditions[0].split(".");
-			FieldSetDefinition fieldSetDefinitionOpLeft = fieldSets.get(fieldSetConditions[0].split("#")[0].trim());
-			FieldSetDefinition fieldSetDefinitionOpRight = fieldSets.get(fieldSetConditions[1].split("#")[0].trim());
-
 			
-			Class fieldsetDataClassLeft = PojoGenerator.generate(fieldSetDefinitionOpLeft.getName(), fieldSetDefinitionOpLeft.getFieldNameToEntityNameMap().keySet());
-			Class fieldsetDataClassRight = PojoGenerator.generate(fieldSetDefinitionOpRight.getName(), fieldSetDefinitionOpRight.getFieldNameToEntityNameMap().keySet());
 
-			List<Record> recordsOfLeftOp = recordsByFieldSet.get(fieldSetDefinitionOpLeft.getName());
-			List fieldsetDataDataList = populateData(fieldsetDataClassLeft, recordsOfLeftOp);
+			Class fieldsetDataClassLeft = generateClassFile(fieldSetConditions[0].split("#")[0].trim());
+			Class fieldsetDataClassRight = generateClassFile(fieldSetConditions[1].split("#")[0].trim());
+			
+			List<Record> recordsOfLeftOp = getRecords(recordsByFieldSet, fieldSetConditions[0].split("#")[0].trim());
+			List fieldsetDataLeft = populateData(fieldsetDataClassLeft, recordsOfLeftOp);
+			List<Record> recordsOfRightOp = getRecords(recordsByFieldSet, fieldSetConditions[1].split("#")[0].trim());
+			List fieldsetDataRight = populateData(fieldsetDataClassRight, recordsOfRightOp);
+
 
 			Map fieldsetDataAtributesLeft = DynamicIndexer.generateAttributesForPojo(fieldsetDataClassLeft);
 			Map fieldsetDataAtributesRight = DynamicIndexer.generateAttributesForPojo(fieldsetDataClassRight);
 			IndexedCollection autoIndexedCollectionLeft = DynamicIndexer.newAutoIndexedCollection(fieldsetDataAtributesLeft.values());
-			autoIndexedCollectionLeft.addAll(fieldsetDataDataList);
 			
+			autoIndexedCollectionLeft.addAll(fieldsetDataLeft);
 
 			IndexedCollection autoIndexedCollectionRight = DynamicIndexer.newAutoIndexedCollection(fieldsetDataAtributesRight.values());
-			autoIndexedCollectionRight.addAll(populateData(fieldsetDataClassRight, recordsByFieldSet.get(fieldSetDefinitionOpRight.getName())));
-			
-			 SimpleAttribute<Object, String> simpleAttribute  = new SimpleAttribute<Object, String>(fieldSetConditions[1].split(".")[1]) {
+			autoIndexedCollectionRight.addAll(fieldsetDataRight);
+
+			SimpleAttribute<Object, String> simpleAttribute = new SimpleAttribute<Object, String>(fieldSetConditions[1].split(".")[1].trim()) {
 				@Override
 				public String getValue(final Object object) {
 					try {
-						return (String) object.getClass().getMethod("get"+fieldSetConditions[1].split(".")[1]).invoke(object, null);
-					} catch (IllegalArgumentException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (SecurityException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (IllegalAccessException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (InvocationTargetException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (NoSuchMethodException e) {
+						return (String) object.getClass().getMethod("get" + fieldSetConditions[1].split("#")[1].trim()).invoke(object, null);
+					} catch (Exception e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 					return null;
 				}
-		    };
-			
-			for(Record record : recordsOfLeftOp){
-				ResultSet resultSet = autoIndexedCollectionRight.retrieve(contains(simpleAttribute,record.getValueByIndexName().get(fieldSetConditions[1].split(".")[1])));
+			};
+
+			for (Record record : recordsOfLeftOp) {
+				String attributeValue = record.getValueByIndexName().get(fieldSetConditions[1].split("#")[1].trim());
+				ResultSet resultSet = autoIndexedCollectionRight.retrieve(contains(simpleAttribute, attributeValue));
 				Iterator iterator = resultSet.iterator();
-				while(iterator.hasNext()){
+				while (iterator.hasNext()) {
 					Object next = iterator.next();
-					
+					createCombinedRecord(records, record, next, fieldSetConditions[1].split("#")[0].trim(),mappingRuleEntry.getKey());
 				}
 			}
-			
+
+			recordsByFieldSet.put(mappingRuleEntry.getKey(), records);
+
 		}
 
-		return null;
+		return records;
 	}
-	@SuppressWarnings({"rawtypes","unchecked"})
-	private List populateData(Class fieldsetDataClass, List<Record> records) throws InstantiationException, IllegalAccessException, IllegalArgumentException, SecurityException, InvocationTargetException, NoSuchMethodException {
+
+	private List<Record> getRecords(Map<String, List<Record>> recordsByFieldSet, final String fieldSetCondition) {
+		FieldSetDefinition fieldSetDefinition= fieldSets.get(fieldSetCondition);
+		List<Record> records = null;
+		if(null != fieldSetDefinition){
+		 records = recordsByFieldSet.get(fieldSetDefinition.getName());
+		}else{
+			records = 	recordsByFieldSet.get(fieldSetCondition);
+		}
+		
+		return records;
+	}
+
+	private Class generateClassFile(String fieldSetName) throws NotFoundException, CannotCompileException {
+		String className = null;
+		Set<String> attributeSet = null;
+
+		FieldSetDefinition fieldSetDefinition = fieldSets.get(fieldSetName);
+		if (null != fieldSetDefinition) {
+			className = fieldSetDefinition.getName();
+			attributeSet = fieldSetDefinition.getFieldNameToEntityNameMap().keySet();
+		}else{
+			className = fieldSetName;
+			attributeSet = attributes.get(fieldSetName);
+		}
+
+		return PojoGenerator.generate(className, attributeSet);
+	}
+
+	private void createCombinedRecord(List<Record> records, Record record, Object next, String string,String mappingName) {
+
+		Map<String, String> map = Maps.newHashMap();
+		map.putAll(record.getValueByIndexName());
+		for (String field : fieldSets.get(string).getFieldNameToEntityNameMap().keySet()) {
+			String value = null;
+			try {
+				value = (String) next.getClass().getMethod("get" + field).invoke(next, null);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			map.put(field, value);
+
+		}
+		attributes.get(mappingName).addAll(map.keySet());
+
+		records.add(new Record(map));
+
+	}
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	private List populateData(Class fieldsetDataClass, List<Record> records) throws InstantiationException, IllegalAccessException, IllegalArgumentException, SecurityException,
+			InvocationTargetException, NoSuchMethodException {
 		List recordsLists = Lists.newArrayList();
 
 		for (Record record : records) {
